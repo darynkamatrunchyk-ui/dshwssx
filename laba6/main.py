22ac0f6b052b6d0a2e26b5c2718f345509d8ab70
darynkamatrunchyk-ui <darynkamatrunchyk@gmail.com>
2025-11-26 09:19:14 +0200

    Update main.py

--- laba6/main.py
+++ laba6/main.py
@@ -2,7 +2,7 @@ from decorator import require_password
 
 @require_password("12345")
 def secret_info():
-    return "This is top-secret information!"
+    return "Це надсекретна інформація!"
 
 @require_password("pass")
 def add(a, b):
